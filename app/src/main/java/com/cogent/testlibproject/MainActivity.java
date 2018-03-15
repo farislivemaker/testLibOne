b75af65d42c4fd79e3773f7b07113fc265ad4234
Cogent dev <cogent.droiddev@gmail.com>
2018-03-15 15:52:29 +0530

    commit on 3rd march

---
+++ app/src/main/java/com/cogent/testlibproject/MainActivity.java
@@ -0,0 +1,21 @@
+package com.cogent.testlibproject;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+
+import com.cogent.libraryone.CentralTendency;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+
+       String a = CentralTendency.concatTwoString("one -","-two");
+       Log.e("Library_Project",a);
+
+    }
+}
